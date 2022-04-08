26901ff847edfb244cc08cfc6fe514e5241d10b5
Les Witherspoon <leswitherspoon@gmail.com>
2022-04-07 23:34:21 -0700

    Refactored code for exercise Step 1.2

--- courseinfo_step2/src/App.js
+++ courseinfo_step2/src/App.js
@@ -9,35 +9,26 @@ const Header = (props) => {
   )
 }
 const Content = (props) => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  return (
-    <div>
-      
-        <Part part={part1} exercises={exercises1}/>
-      
-        <Part part = {part2} exercises = {exercises2} />
-     
-        <Part part = {part3} exercises = {exercises3} />
-     
-    </div>
-  )
-}
-
-const Part = (props) => {
+  // const part1 = 'Fundamentals of React'
+  // const exercises1 = 10
+  // const part2 = 'Using props to pass data'
+  // const exercises2 = 7
+  // const part3 = 'State of a component'
+  // const exercises3 = 14
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
       </p>
     </div>
   )
 }
-
 
 const Total = (props) => {
   // const part1 = 'Fundamentals of React'
@@ -66,8 +57,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      {/* <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/> */}
-      <Content />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       {/* <p>Number of exercises {exercises1 + exercises2 + exercises3}</p> */}
       <Total part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
     </div>
